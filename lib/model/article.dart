c9a78672c1653595fa617598b24228386193f1a3
CodeZ3 <shifanifathima333@gmail.com>
2024-12-17 07:16:18 +0530

    Update

--- lib/model/article.dart
+++ lib/model/article.dart
@@ -23,13 +23,13 @@ class Article {
 
   factory Article.fromJson(Map<String, dynamic> json) {
     return Article(
-      source: Source.fromJson(json['source']),
+      source: json['source'] != null ? Source.fromJson(json['source']) : Source.defaultSource(), // Handle null case
       author: json['author'] ?? 'Unknown Author',
-      title: json['title'],
+      title: json['title'] ?? 'No Title', // Provide a default value
       description: json['description'] ?? '',
-      url: json['url'],
+      url: json['url'] ?? '', // Provide a default value
       urlToImage: json['urlToImage'] ?? '',
-      publishedAt: json['publishedAt'],
+      publishedAt: json['publishedAt'] ?? '', // Provide a default value
       content: json['content'] ?? '',
     );
   }
--- lib/model/source.dart
+++ lib/model/source.dart
@@ -2,15 +2,17 @@ class Source {
   String id;
   String name;
 
-  Source({
-    required this.id,
-    required this.name,
-  });
+  Source({required this.id, required this.name});
 
   factory Source.fromJson(Map<String, dynamic> json) {
     return Source(
-      id: json['id'] ?? '',
-      name: json['name'],
+      id: json['id'] ?? '', // Provide a default value
+      name: json['name'] ?? 'Unknown Source', // Provide a default value
     );
+  }
+
+  // Default Source
+  static Source defaultSource() {
+    return Source(id: '', name: 'Unknown Source');
   }
 }
